2e22b0e1faa9e4fa7ed644205c188728500da49f
surysaroch <suryanshsaroch5@gmail.com>
2025-10-20 12:45:47 -0400

    projects and homepage file

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,12 @@
+import Projects from "./Projects"
+
+const Homepage = () => {
+    return (
+        <div className="dashboard-container">
+            <Projects />
+        </div>
+    )
+}
+
+export default Homepage
+            
---
+++ src/components/Projects.jsx
@@ -0,0 +1,8 @@
+const Projects = () => {
+    return (
+        <div className="projects-section">
+            <h2>My Projects</h2>
+            <p>yezking</p>
+        </div>
+    )}
+export default Projects
